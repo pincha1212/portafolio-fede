2a4cace22bf633a9edf39f75278bb87ce43580ba
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2024-12-30 19:20:01 -0300

    eliminar SeccionesComponent de los imports en AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from "./components/home/home.component";
-import { SeccionesComponent } from "./components/secciones/secciones.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, SeccionesComponent],
+  imports: [HomeComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
