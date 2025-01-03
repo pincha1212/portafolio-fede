2fcc6fc6778e544c192d8fb0548ba9501073cb07
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2025-01-02 21:15:58 -0300

    mejora la estructura y estilo de los componentes, actualiza textos y añade animaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import { SeccionesComponent } from './components/secciones/secciones.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import {
+  APP_BASE_HREF,
+  HashLocationStrategy,
+  LocationStrategy,
+} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [SeccionesComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/',
+    },
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
