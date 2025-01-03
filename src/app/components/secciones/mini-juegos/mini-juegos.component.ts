a9ae79aab6a670bfa566f2f371bb6fa3a14c5899
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2025-01-03 18:34:11 -0300

    añade funcionalidad y estilos al componente "Mini Juegos" con modal para el minijuego

--- src/app/components/secciones/mini-juegos/mini-juegos.component.ts
+++ src/app/components/secciones/mini-juegos/mini-juegos.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './mini-juegos.component.css'
 })
 export class MiniJuegosComponent {
+  openModalBtn!: Element | null;
+  modal!: HTMLElement | null;
+  closeModalBtn!: Element | null;
 
+  ngOnInit() {
+    this.openModalBtn = document.querySelector('.open-modal-btn');
+    this.modal = document.getElementById('gameModal');
+    this.closeModalBtn = document.querySelector('.close-btn');
+
+    this.openModalBtn?.addEventListener('click', () => {
+      if (this.modal) {
+        this.modal.style.display = 'flex';
+      }
+    });
+
+    this.closeModalBtn?.addEventListener('click', () => {
+      if (this.modal) {
+        this.modal.style.display = 'none';
+      }
+    });
+
+    window.addEventListener('click', (event) => {
+      if (event.target === this.modal && this.modal) {
+        this.modal.style.display = 'none';
+      }
+    });
+  }
 }
