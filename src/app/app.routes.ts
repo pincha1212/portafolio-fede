083b29020b5dc68db9a55dad3cec63a4f9a6f31e
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2025-01-05 09:33:44 -0300

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,53 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ContactoComponent } from './components/secciones/contacto/contacto.component';
-import { ExperienciaComponent } from './components/secciones/experiencia/experiencia.component';
-import { FormacionComponent } from './components/secciones/formacion/formacion.component';
-import { SobremiComponent } from './components/secciones/sobremi/sobremi.component';
-import { ProyectoComponent } from './components/secciones/proyecto/proyecto.component';
-import { MiniJuegosComponent } from './components/secciones/mini-juegos/mini-juegos.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
-  { path: 'home', component: HomeComponent }, 
-  { path: 'sobremi', component: SobremiComponent },
-  { path: 'proyecto', component: ProyectoComponent },
-  { path: 'formacion', component: FormacionComponent },
-  { path: 'experiencia', component: ExperienciaComponent },
-  { path: 'contacto', component: ContactoComponent },
-  { path: 'minijuegos', component: MiniJuegosComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'sobremi',
+    loadComponent: () =>
+      import('./components/secciones/sobremi/sobremi.component').then(
+        (m) => m.SobremiComponent
+      ),
+  },
+  {
+    path: 'proyecto',
+    loadComponent: () =>
+      import('./components/secciones/proyecto/proyecto.component').then(
+        (m) => m.ProyectoComponent
+      ),
+  },
+  {
+    path: 'formacion',
+    loadComponent: () =>
+      import('./components/secciones/formacion/formacion.component').then(
+        (m) => m.FormacionComponent
+      ),
+  },
+  {
+    path: 'experiencia',
+    loadComponent: () =>
+      import('./components/secciones/experiencia/experiencia.component').then(
+        (m) => m.ExperienciaComponent
+      ),
+  },
+  {
+    path: 'contacto',
+    loadComponent: () =>
+      import('./components/secciones/contacto/contacto.component').then(
+        (m) => m.ContactoComponent
+      ),
+  },
+  {
+    path: 'minijuegos',
+    loadComponent: () =>
+      import('./components/secciones/mini-juegos/mini-juegos.component').then(
+        (m) => m.MiniJuegosComponent
+      ),
+  },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }, // Ruta comodín para manejar errores
 ];
