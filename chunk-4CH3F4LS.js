7a306697d11479ba72d28a444050de086fe6dff4
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2025-01-06 01:58:33 -0300

    Auto-generated commit

---
+++ chunk-4CH3F4LS.js
@@ -0,0 +1,11 @@
+import{L as a}from"./chunk-WVTPRLOA.js";var s=class e{blogPosts=[{id:"como-manejar-rutas-anidadas-angular",title:"C\xF3mo manejar rutas anidadas en Angular",description:"Aprende a configurar rutas anidadas paso a paso.",content:`
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod velit vel lacus gravida, id scelerisque risus 
+      pretium. Suspendisse potenti. Integer viverra lorem ac libero convallis, ac laoreet elit tincidunt. In nec libero sit 
+      amet lorem laoreet tincidunt ut id magna. Proin vel consequat ipsum, eu malesuada arcu. Aliquam erat volutpat. Curabitur 
+      ac libero purus. Ut vitae venenatis metus, sit amet gravida tortor. Suspendisse id nisl nisl. Donec a ligula vel lorem 
+      aliquam eleifend. Fusce non felis a ante luctus ultricies ac sit amet lorem.
+      
+      Sed laoreet auctor purus, non gravida libero convallis ut. Vivamus euismod nisi at risus consequat, sit amet pretium 
+      metus elementum. Mauris aliquet, ipsum vel feugiat rhoncus, augue urna scelerisque eros, eget aliquet velit ipsum vitae 
+      nunc. Nulla fringilla nec felis ac fermentum. Etiam feugiat odio leo, a volutpat magna auctor vitae. Morbi feugiat 
+      nunc id elit vestibulum, non tempus urna maximus. Donec viverra leo quis odio pharetra, a tincidunt lorem luctus.`},{id:"mejorando-accesibilidad-proyectos-web",title:"Mejorando la accesibilidad en tus proyectos web",description:"Consejos para interfaces accesibles y amigables.",content:"Contenido completo del post sobre accesibilidad."},{id:"introduccion-typescript",title:"Introducci\xF3n a TypeScript",description:"Gu\xEDa b\xE1sica para empezar con TypeScript.",content:"Ut suscipit eros sed nisl tempus, vitae consectetur risus vehicula. Donec mollis consectetur sapien vel consequat. Integer ut nulla sit amet sapien gravida pretium nec vel justo."}];constructor(){}getPosts(){return this.blogPosts}getPostById(t){return this.blogPosts.find(i=>i.id===t)}static \u0275fac=function(i){return new(i||e)};static \u0275prov=a({token:e,factory:e.\u0275fac,providedIn:"root"})};export{s as a};
